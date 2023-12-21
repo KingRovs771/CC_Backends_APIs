9991586dadbe5f8271e50e444f39cd003cd3863c
Rizky Budiarto <53458876+rizkybudi2826@users.noreply.github.com>
2023-12-21 17:46:37 +0700

    Ubah Server

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ routes(app);
 
 app.use('/auth', require('./middleware/index'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server started on port`);
 });
